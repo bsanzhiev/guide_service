bc593ac6883f910ccc578896cc57fff90a713793
bator <bsanzhiev@outlook.com>
2025-02-10 20:29:13 +0800

    added place service interface

---
+++ ports/place_service.go
@@ -0,0 +1,15 @@
+package ports
+
+import "github.com/bsanzhiev/guide_service/domain"
+
+type PlaceService interface {
+	CreatePlace(p domain.Place) (domain.Place, error)
+
+	GetPlaceByID(id string) (domain.Place, error)
+
+	UpdatePlace(id string, p domain.Place) (domain.Place, error)
+
+	DeletePlace(id string) error
+
+	GetAllPlace() ([]domain.Place, error)
+}
